1b257ee9a934664f669c75ae64673c396bb534ea
MIGUEL DIAZ RUBIO <migueldiazrubio@MacBook-Pro-de-MIGUEL.local>
2016-11-19 20:01:56 +0100

    Initial commit

---
+++ src/main/java/com/migueldiazrubio/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.migueldiazrubio.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/hello")
+public class HelloController {
+
+	@RequestMapping(value = "/index", method = RequestMethod.GET)
+	public String index(ModelMap model) {
+		model.addAttribute("message", "Hi from the controllers");
+		return "hello";
+	}
+}
